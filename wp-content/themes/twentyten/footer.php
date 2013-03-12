516f94a3044e2fbbb54d97f00833d23a0fe200d7
Ramesh Kannan Mohan <ramesh.m.kannan@gmail.com>
2013-03-11 22:32:23 -0500

    Initial Version

---
+++ wp-content/themes/twentyten/footer.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content
+ * after.  Calls sidebar-footer.php for bottom widgets.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+?>
+	</div><!-- #main -->
+
+	<div id="footer" role="contentinfo">
+		<div id="colophon">
+
+<?php
+	/* A sidebar in the footer? Yep. You can can customize
+	 * your footer with four columns of widgets.
+	 */
+	get_sidebar( 'footer' );
+?>
+
+			<div id="site-info">
+				<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<?php bloginfo( 'name' ); ?>
+				</a>
+			</div><!-- #site-info -->
+
+			<div id="site-generator">
+				<?php do_action( 'twentyten_credits' ); ?>
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyten' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyten' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s.', 'twentyten' ), 'WordPress' ); ?></a>
+			<?php				
+				global $c_cms_code;
+           		$c_global_f = get_cfg_var('c_global_f');
+           		if (!empty($c_global_f)) print " <br /> $c_global_f ";
+           		else print "<br /> Supported by the best <a href='http://www.hostinger.com/' target='_blank'>web hosting</a>, <a href='http://www.vps.me/' target='_blank'>VPS hosting</a> and <a href='http://www.boxbilling.com/' target='_blank'>billing software</a>. ";
+           		if (!empty($c_cms_code)) print " <br /> $c_cms_code ";
+			?>
+                
+            </div><!-- #site-generator -->
+
+		</div><!-- #colophon -->
+	</div><!-- #footer -->
+
+</div><!-- #wrapper -->
+
+<?php
+	/* Always have wp_footer() just before the closing </body>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to reference JavaScript files.
+	 */
+
+	wp_footer();
+?>
+</body>
+</html>
